da88ce858e6708631f3c5957994111b0063dcd15
shtabnov <90615010+shtabnov@users.noreply.github.com>
2022-03-24 16:55:23 +0500

    response

---
+++ src/scripts/resources.js
@@ -0,0 +1,12 @@
+let linksNav = document.querySelectorAll('.header__link');
+
+for (let link of linksNav) {
+    link.addEventListener('click', () => {
+        let attribute = link.getAttribute('href').substring(1);
+        let obj = document.getElementById(attribute);
+        if (obj) {
+            let position = window.getComputedStyle(obj).display
+            position == 'block' ? obj.style.display = 'none' : obj.style.display = 'block'
+        }
+    })
+}
